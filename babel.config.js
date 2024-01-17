c4a8d67a6f8594ed0d3d9e44bae73c1767641ef2
Sagar Monga <sagar.monga@grapecity.com>
2024-01-17 14:29:34 +0530

    feat: integrate @ imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@redux': './src/redux',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@types': './src/types',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
