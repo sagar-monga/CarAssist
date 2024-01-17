5f918bae35d7956b9b028f1e4ec22677a999c2ad
Sagar Monga <sagar.monga@grapecity.com>
2024-01-17 18:33:13 +0530

    chore: update formatter settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
-};
+  bracketSpacing: true,
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
+ };
